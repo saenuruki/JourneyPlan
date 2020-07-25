42e1d297b40be64fb68930482215374e6b65741a
“saenuruki” <sae.webwork@gmail.com>
2020-07-25 12:12:45 +0900

    create project

---
+++ JourneyPlan/JourneyPlan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JourneyPlan
+//
+//  Created by 塗木冴 on 2020/07/25.
+//  Copyright © 2020 塗木冴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
